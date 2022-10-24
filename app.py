c22477853e80a33e9cadb94668a436db09d9df9a
Sfumato 92 <sfumato000@gmail.com>
2022-10-24 03:06:33 -0700

    Implement strike selection logic

--- app.py
+++ app.py
@@ -3,35 +3,34 @@ import pandas as pd
 
 MINUTES_IN_A_YEAR = 525600  # number of minutes in a year
 
+pd.set_option('display.max_columns', 12)
+pd.set_option('display.width', 1000)
+
 
 def process(df):
-    # df = df.sort_values(by="option_symbol", ascending=True)
-    df[["Symbol", "Settlement", "Strike", "Call/Put"]] = df["option_symbol"].str.split(
+    df[["Symbol", "Settlement", "Strike", "C/P"]] = df["option_symbol"].str.split(
         "-", expand=True
     )
-
+    df.drop("option_symbol", inplace=True, axis=1)
+    df = df[
+        ["Symbol", "Settlement", "Strike", "C/P", "ask1", "bid1", "mark_price"]
+    ]
     df["Settlement"] = "20" + df["Settlement"].astype(str)
     df["Settlement"] = pd.to_datetime(df["Settlement"], format="%Y%m%d")
     df["Strike"] = df["Strike"].astype(float)
-    df["strike"] = df["Strike"]
-    # df = df.sort_values(by="strike", ascending=True)
     return df
 
 
 def filter_data(df: pd.DataFrame, curr_date=datetime.now()):
-    ret = df.copy()
+    ret = df
 
     # T window
     ret = ret.loc[
         ((ret["Settlement"] - curr_date).dt.days >= 23)
         & ((ret["Settlement"] - curr_date).dt.days <= 37)
-    ]
+        ]
 
-    # 0 bid/ask
-    # ret = ret.loc[(ret["bid1"] > 0) & (ret["ask1"] > 0)]
-
-    # remove all
-    return ret.sort_values(["strike"], ascending=True)
+    return ret.sort_values(["Symbol", "Settlement", "Strike"], ascending=True)
 
 
 def cal_t_value():
@@ -39,20 +38,40 @@ def cal_t_value():
 
 
 def cal_f_value(groups) -> float:
-
     # print("Call :\n", groups.first())
     # print("Put :\n", groups.nth(1))
 
     f_values = (
-        groups.first()["strike"]
-        + groups.first()["mark_price"]
-        - groups.nth(1)["mark_price"]
+            groups.first()["strike"]
+            + groups.first()["mark_price"]
+            - groups.nth(1)["mark_price"]
     )
     return f_values
 
 
-def main():
+# remove illiquid strikes
+def select_strikes(df: pd.DataFrame) -> pd.DataFrame:
+    df["min_b/a"] = df.apply(lambda x: min(x.bid1, x.ask1), axis=1)
+    df["min_b/a_diff"] = df.groupby(["Symbol", "Settlement", "C/P"], sort=False)[["min_b/a"]].diff()
 
+    df_call = df[df["C/P"] == "C"]
+    df_put = df[df["C/P"] == "P"]
+    df_put = df_put[df_put.groupby(["Symbol", "Settlement"], sort=False)["min_b/a_diff"].transform(
+        lambda x: x.ne(0).cumprod().astype(bool))]
+    df_call = df_call.sort_values(by=["Symbol", "Settlement", "Strike"], ascending=[True, True, False])
+    df_call = df_call[df_call.groupby(["Symbol", "Settlement"], sort=False)["min_b/a_diff"].transform(
+        lambda x: x.ne(0).cumprod().astype(bool))]
+    print(df_call)
+    df = pd.concat([df_call, df_put], axis=0)
+
+    # remove 0 bid/ask strikes
+    df = df[df["min_b/a"] > 0]
+    df.sort_values(["Symbol", "Settlement", "Strike"], ascending=True, inplace=True)
+    print(df)
+    return df
+
+
+def main():
     date1 = datetime.strptime("20221005", "%Y%m%d")
     file_path = "./data/sample/01.csv"
 
@@ -62,16 +81,19 @@ def main():
     df = process(df)
     # print("Processed data :\n", df)
 
-    t_df = filter_data(df, date1)
-    # print("Selected data by the given date :\n", t_df)
+    df = filter_data(df, date1)
+    print("Selected data by the given date :\n", df)
 
-    groups = t_df.groupby(["Symbol", "Settlement", "Strike"], group_keys=False)
-    print("Group :\n")
-    for key, item in groups:
-        print(item)
+    df = select_strikes(df)
+    # print("df :\n", df)
 
-    f_values = cal_f_value(groups)
-    print("F values : \n", f_values)
+    # groups = df.groupby(["Symbol", "Settlement", "C/P"], sort=False)
+    # print("Group :\n")
+    # for key, item in groups:
+    #     print(item)
+
+    df.drop(["min_b/a", "min_b/a_diff"], inplace=True, axis=1)
+    print("df :\n", df)
 
 
 if __name__ == "__main__":
